846c06e15133b285b3151c7652b75ab741b5dceb
alexanderbarroso <pander_64@hotmail.com>
2022-10-09 15:35:58 -0500

    Changes for ecommerce app

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -18,10 +18,10 @@ export default withAuth(
   // Using the config function helps typescript guide you to the available options.
   config({
     // the db sets the database provider - we're using sqlite for the fastest startup experience
-    db: {
-      provider: 'sqlite',
-      url: 'file:./keystone.db',
-    },
+      db: {
+          provider: 'postgresql',
+          url: 'postgres://abarroso@localhost:5432/keystones'
+      },
     // This config allows us to set up features of the Admin UI https://keystonejs.com/docs/apis/config#ui
     ui: {
       // For our starter, we check that someone has session data before letting them see the Admin UI.
