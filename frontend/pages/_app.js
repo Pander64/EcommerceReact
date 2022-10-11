dd499417d73c05ac9cb381dd968582158de9f96b
alexanderbarroso <pander_64@hotmail.com>
2022-10-11 00:35:04 -0500

    Changes

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,9 @@
+import { ApolloProvider } from '@apollo/client';
 import NProgress from 'nprogress';
 import Router from 'next/router';
 import Page from '../components/Page';
 import '../components/styles/nprogress.css';
+import withData from '../lib/withData';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
