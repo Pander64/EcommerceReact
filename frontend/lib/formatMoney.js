1ab8fa0050edfb1009819bc705512ecf01824c26
Alexander Barroso <pander_64@hotmail.com>
2022-10-25 03:03:30 -0500

    File 25 and video 27

---
+++ frontend/lib/formatMoney.js
@@ -0,0 +1,12 @@
+export default function FormatMoney(amount = 0) {
+
+    const options = {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+    }
+
+    const formatter = new Intl.NumberFormat('en.US', options);
+
+    return formatter.format(amount / 100);
+}
